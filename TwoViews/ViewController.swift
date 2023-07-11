eeabf4df83285230f0173d0f50063a0d87773391
Matvei95 <khlestov.95@mail.ru>
2023-07-11 11:02:14 +0500

    Сode optimization

--- TwoViews/ViewController.swift
+++ TwoViews/ViewController.swift
@@ -42,9 +42,18 @@ class ViewController: UIViewController {
     private func layoutGreenView() {
         NSLayoutConstraint.activate(
             [
-                greenView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-                greenView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-                greenView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+                greenView.topAnchor.constraint(
+                    equalTo: view.safeAreaLayoutGuide.topAnchor,
+                    constant: 20
+                ),
+                greenView.leadingAnchor.constraint(
+                    equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                    constant: 16
+                ),
+                greenView.trailingAnchor.constraint(
+                    equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                    constant: -16
+                ),
                 greenView.heightAnchor.constraint(equalToConstant: 312)
             ]
         )
@@ -53,10 +62,22 @@ class ViewController: UIViewController {
     private func layoutRedView() {
         NSLayoutConstraint.activate(
             [
-                redView.topAnchor.constraint(equalTo: greenView.bottomAnchor, constant: 24),
-                redView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-                redView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-                redView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+                redView.topAnchor.constraint(
+                    equalTo: greenView.bottomAnchor,
+                    constant: 24
+                ),
+                redView.leadingAnchor.constraint(
+                    equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                    constant: 16
+                ),
+                redView.trailingAnchor.constraint(
+                    equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                    constant: -16
+                ),
+                redView.bottomAnchor.constraint(
+                    equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                    constant: -20
+                ),
                 redView.heightAnchor.constraint(equalTo: greenView.heightAnchor)
             ]
         )
